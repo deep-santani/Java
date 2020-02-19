a3d2c5b01872fe54dcd48c9203366520a77a7852
Deep santani <dipsantani789@gmail.com>
2020-02-20 01:07:54 +0530

    Looping in Java
    In java use the advance loop name as FOR(int i : Array_name). 
    The Java for loop perform quick operation using less Argument.

---
+++ 7.Looping in java/Test.java
@@ -0,0 +1,11 @@
+class Test
+{
+    public static void main(String[] args) 
+    {
+      int a[]={10,20,30,40,50,60,70,80,90,100};
+      for(int i:a)
+      {
+          System.out.println(i);
+      }   
+    }
+}
