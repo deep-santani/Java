f90f24f23efdad8d7dabdd48f286e82f47d02b07
Deep santani <dipsantani789@gmail.com>
2020-02-29 10:50:03 +0530

    Outter & Inner Class
    Outter Inner class is a very important concept.
    When the Outter Class Object is not Created then not possible to Create Inner Class Object.

---
+++ 16.Outter Inner Class/Gtu.java
@@ -0,0 +1,22 @@
+//If Outter class is not Exist then inner class is not Possible in entire life Time//
+//Hear If GTU is not then ADIT is not Exist//
+
+
+public class Gtu
+{
+    class Adit
+    {
+      public Adit()
+      {  
+      System.out.println("Follow the Group meme4adiitians");
+      }
+    }
+}
+class Main
+{
+    public static void main(String[] args) {
+        
+        Gtu Selfearning=new Gtu();
+        Gtu.Adit It = new Gtu().new Adit();
+    }
+}
